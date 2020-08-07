f2135b223134e8bdb925054318f8d0ade662ea16
rashedhero <rashedbefresh@gmail.com>
2020-08-07 22:20:55 +0600

    array-slice

---
+++ array-slice.js
@@ -0,0 +1,17 @@
+
+const nums=[2,4,6,8,9,5];
+
+const part=nums.slice(2,5);
+console.log(part);
+
+
+
+
+
+const remove=nums.splice(3,2)
+
+const together =nums.join(", ");
+
+console.log(remove);
+console.log(nums);
+console.log(together);
