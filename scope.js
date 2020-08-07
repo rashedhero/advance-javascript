6944b19356a6c92f1531e159cd1910c06e8f6a71
rashedhero <rashedbefresh@gmail.com>
2020-08-07 22:00:04 +0600

    edit closure

---
+++ scope.js
@@ -0,0 +1,16 @@
+
+let bonus=30,
+
+function sum (num1,num2) {
+   
+    result = num1+num2+bonus;
+    console.log(bonus);
+    return result;
+}
+
+
+
+
+console.log(bonus);
+var result=sum(5,9);
+console.log(result);
